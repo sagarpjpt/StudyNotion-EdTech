c45dabd9310aec770aa3df3e43190dcc5a3c5a50
KattarCoder <sagarprjpt99@gmail.com>
2025-12-06 22:01:32 +0530

    fix import issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+// import VitePluginChecker from "vite-plugin-checker";
+
+
 
 export default defineConfig(({ mode }) => {
   // Load environment variables from .env files
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react(),
-    tailwindcss()
+    plugins: [
+      react(),
+      tailwindcss(),
     ],
     server: {
       port: parseInt(env.VITE_PORT) || 5173, //  Safe access
     },
-  }
-})
+  };
+});
